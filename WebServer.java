cd434d5a477972a7d0b3ae2945527c8326f81770
reineB5 <breine8@gmail.com>
2026-04-16 22:05:18 +0300

    Finish web server implementation

--- WebServer.java
+++ WebServer.java
@@ -134,4 +134,37 @@ class HttpRequest implements Runnable {
         br.close();
         socket.close();
     }
+
+    // Send file bytes to client
+private static void sendBytes(FileInputStream fis, OutputStream os) throws Exception {
+    byte[] buffer = new byte[1024];
+    int bytes = 0;
+
+    while ((bytes = fis.read(buffer)) != -1) {
+        os.write(buffer, 0, bytes);
+    }
+}
+
+// Determine content type (MIME)
+private static String contentType(String fileName) {
+    if (fileName.endsWith(".htm") || fileName.endsWith(".html")) {
+        return "text/html";
+    }
+    if (fileName.endsWith(".jpg") || fileName.endsWith(".jpeg")) {
+        return "image/jpeg";
+    }
+    if (fileName.endsWith(".gif")) {
+        return "image/gif";
+    }
+    if (fileName.endsWith(".png")) {
+        return "image/png";
+    }
+    if (fileName.endsWith(".css")) {
+        return "text/css";
+    }
+    if (fileName.endsWith(".js")) {
+        return "application/javascript";
+    }
+    return "application/octet-stream";
+}
 }
