b37121ce73cf32720655e030334a7788fbe99d27
Alfa-Zer-0 <134849383+Alfa-Zer-0@users.noreply.github.com>
2026-04-25 20:33:01 +0300

    Refactor UnreliableChannel for clarity and comments

--- UnreliableChannel.java
+++ UnreliableChannel.java
@@ -2,13 +2,13 @@ import java.net.*;
 import java.util.Random;
 
 public class UnreliableChannel {
-
     public static void main(String[] args) throws Exception {
-        
-        int port = Integer.parseInt(args[0]);
-        double p = Double.parseDouble(args[1]);
-        int minD = Integer.parseInt(args[2]);
-        int maxD = Integer.parseInt(args[3]);
+
+        int port = Integer.parseInt(args[0]);   // listening port
+        double p = Double.parseDouble(args[1]); // loss probability
+        int minD = Integer.parseInt(args[2]);   // min delay (ms)
+        int maxD = Integer.parseInt(args[3]);   // max delay (ms)
+
         String[] user = new String[2];
         InetAddress[] IP = new InetAddress[2];
         int[] userPort = new int[2];
@@ -19,7 +19,8 @@ public class UnreliableChannel {
         boolean[] ends = new boolean[2];
         user[0] = "A";
         user[1] = "B";
-        
+
+        // register destination
         String dest = args[4];
         int c = dest.lastIndexOf(':');
         IP[1] = InetAddress.getByName(dest.substring(0, c));
@@ -27,10 +28,10 @@ public class UnreliableChannel {
 
         Random rnd = new Random();
         DatagramSocket socket = new DatagramSocket(port);
-        byte[] buffer = new byte[4096];
+        byte[] buffer = new byte[4096]; 
 
         while (true) {
-
+            //wait for incoming packet
             DatagramPacket dp = new DatagramPacket(buffer, buffer.length);
             socket.receive(dp);
 
@@ -38,23 +39,23 @@ public class UnreliableChannel {
             InetAddress senderIP = dp.getAddress();
             int senderPort = dp.getPort();
 
+            // match sender by IP & port
             int index = -1;
             for (int i = 0; i < 2; i++) {
-            if (IP[i] != null && IP[i].equals(senderIP) && userPort[i] == senderPort) {
-                index = i;
-                break;
+                if (IP[i] != null && IP[i].equals(senderIP) && userPort[i] == senderPort) {
+                    index = i;
+                    break;
                 }
             }
-
+            // register new sender if not matched
             if (index == -1) {
                 if (IP[0] == null) index = 0;
                 else if (IP[1] == null) index = 1;
                 else continue;
-
                 IP[index] = senderIP;
                 userPort[index] = senderPort;
             }
-
+            // "END" message handling
             if (message.equals("END")) {
                 ends[index] = true;
                 if (ends[0] && ends[1]) {
@@ -64,34 +65,35 @@ public class UnreliableChannel {
                 }
                 continue;
             }
-
             received[index]++;
-
             if (rnd.nextDouble() <= p) {
+                // drop packet with probability p
                 lost[index]++;
             } else {
-                int destIndex = 1 - index;
+                int destIndex = 1 - index; // the other party
                 if (IP[destIndex] == null) {
                     lost[index]++;
                     continue;
                 }
-                int delay = (maxD > minD) ? minD + rnd.nextInt(maxD - minD + 1) : minD;
-                Thread.sleep(delay);
 
-
-                byte[] outData = new byte[dp.getLength()];
-                System.arraycopy(dp.getData(), 0, outData, 0, dp.getLength());
-                socket.send(new DatagramPacket(outData, outData.length, IP[destIndex], userPort[destIndex]));
-                delayed[index]++;
-                totalDelay[index] += delay;
+            // random delay 
+            int delay = (maxD > minD) ? minD + rnd.nextInt(maxD - minD + 1) : minD;
+            Thread.sleep(delay);
+            
+            //copy & forward packet
+            byte[] outData = new byte[dp.getLength()];
+            System.arraycopy(dp.getData(), 0, outData, 0, dp.getLength());
+            socket.send(new DatagramPacket(outData, outData.length, IP[destIndex], userPort[destIndex]));
+            delayed[index]++;
+            totalDelay[index] += delay;
             }
         }
     }
 
+
     private static void printStats(String[] user, int[] received, int[] lost, int[] delayed, long[] totalDelay) {
         for (int i = 0; i < 2; i++)
             System.out.println("Packets received from user " + user[i] + ": " + received[i] + " | Lost: " + lost[i] + " | Delayed: " + delayed[i]);
-
         System.out.printf("Average delay from %s to %s: %.1f ms.%n", user[0], user[1], delayed[0] > 0 ? (double) totalDelay[0] / delayed[0] : 0.0);
         System.out.printf("Average delay from %s to %s: %.1f ms.%n", user[1], user[0], delayed[1] > 0 ? (double) totalDelay[1] / delayed[1] : 0.0);
     }
